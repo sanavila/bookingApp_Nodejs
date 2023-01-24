e3988da5876295bcd38a65bd89b91452161af9b9
sanavila <rute.leone@hotmail.com>
2023-01-24 11:27:33 -0300

    create routes using express.Router()

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import authRoute from './api/routes/auth.js'
+import hotelsRoute from './api/routes/hotels.js'
+import roomsRoute from './api/routes/rooms.js'
+import usersRoute from './api/routes/users.js'
 
 dotenv.config()
 const app = express()
 
-mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true)
 
 const connect = async () => {
   try {
@@ -20,13 +24,11 @@ mongoose.connection.on("disconnected", () => {
   console.log("MongoDB disconnected!")
 })
 
-mongoose.connection.on("connected", () => {
-  console.log("MongoDB connected!")
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello first request")
-})
+//middlewares
+app.use("/api/auth", authRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/rooms", roomsRoute)
+app.use("/api/users", usersRoute)
 
 app.listen(8800, () => {
   connect()
