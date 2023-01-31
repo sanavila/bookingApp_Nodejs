d2bd72c06dce81461e74060289acbd4d228f2c98
sanavila <rute.leone@hotmail.com>
2023-01-31 13:29:55 -0300

    API hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,8 +16,55 @@ router.post("/", async (req, res) => {
   }
 })
 //UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body},
+      { new: true}
+    )
+    res.status(200).json(updateHotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 //DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(
+      req.params.id
+    )
+    res.status(200).json("Hotel has been deleted.")
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 //GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(
+      req.params.id
+    )
+    res.status(200).json(hotel)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 //GET ALL
+router.get("/", async (req, res, next) => {
+
+  const failed = true
+  const err = new Error()
+  err.status = 404
+  err.message = "Sorry not found!"
+  if (failed) return next(err)
+
+  try {
+    const hotels = await Hotel.findById("2222")
+    res.status(200).json(hotels)
+  } catch (err) {
+    next(err)
+  }
+})
 
 export default router
